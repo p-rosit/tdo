fb898b6af8ffcfcb21d224d5b69411da167db624
p-rosit <pontus.rosqvist@gmail.com>
2026-03-25 18:00:07 +0100

    parse arguments and setup arena

--- src/runner.c
+++ src/runner.c
@@ -1,5 +1,23 @@
 #include "string.c"
+#include "arguments.c"
 
-int main() {
-    return TDO_ARENA_ALIGNMENT;
+int main(int argc, char **argv) {
+    enum TdoError result = TDO_ERROR_UNKNOWN;
+
+    struct TdoArguments args;
+    result = tdo_arguments_parse(&args, argc, argv);
+    if (result != TDO_ERROR_OK) goto error_parse;
+
+    struct TdoArena *arena = tdo_arena_init(1024);
+    if (arena == NULL) return -1;
+    struct TdoArena *string_arena = tdo_arena_init(4);
+    if (string_arena == NULL) {
+        tdo_arena_deinit(arena);
+        return -1;
+    }
+
+    tdo_arena_deinit(string_arena);
+    tdo_arena_deinit(arena);
+    error_parse:
+    return result;
 }
