0d843a5866409253d0425a2f2b33fdb8e1deb139
p-rosit <pontus.rosqvist@gmail.com>
2026-04-07 15:21:56 +0200

    stub implementation for windows

---
+++ src/platform/windows.c
@@ -0,0 +1,26 @@
+#include <windows.h>
+#include "interface.h"
+
+struct TdoReadResult tdo_read_fd(TdoFileDescriptor fd, size_t size, char *buffer);
+
+void tdo_write_fd(TdoFileDescriptor fd, size_t size, char const *data);
+
+TdoMonotoneTime tdo_time_get(void);
+
+struct TdoLibraryLoadResult tdo_dynamic_library_load(char const *path, struct TdoArena *arena);
+
+void tdo_dynamic_library_unload(TdoLibrary lib);
+
+struct TdoSymbolLoadResult tdo_dynamic_symbol_load(TdoLibrary lib, char const *name, struct TdoArena *arena);
+
+bool tdo_process_status_is_exit(TdoProcessStatus status);
+
+bool tdo_process_status_is_signal(TdoProcessStatus status);
+
+bool tdo_process_status_is_stop(TdoProcessStatus status);
+
+TdoProcessCode tdo_process_code_exit(TdoProcessStatus status);
+
+TdoProcessCode tdo_process_code_signal(TdoProcessStatus status);
+
+TdoProcessCode tdo_process_code_stop(TdoProcessStatus status);
