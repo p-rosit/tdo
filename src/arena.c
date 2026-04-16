e4de31b3865442bad9ff7db2524af3744b243485
Pontus <pontus.rosqvist@gmail.com>
2026-04-16 17:12:23 +0200

    extract arena header

--- src/arena.c
+++ src/arena.c
@@ -3,44 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
-
-#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 202311L
-    #include <stddef.h>
-    #define TDO_ARENA_ALIGNMENT alignof(max_align_t)
-#elif defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
-    #include <stddef.h>
-    #define TDO_ARENA_ALIGNMENT _Alignof(max_align_t)
-#else
-    #include <stddef.h>
-
-    union MaxAlign {
-        void *p;
-        long l;
-        double d;
-        long double ld;
-        long long ll;
-        void (*f)(void);
-    };
-
-    struct MaxAlignHelper {
-        char a;
-        union MaxAlign b;
-    };
-
-    #define TDO_ARENA_ALIGNMENT offsetof(struct MaxAlignHelper, b)
-#endif
-
-struct TdoArenaNode {
-    struct TdoArenaNode *next;
-    char *current;
-    char *end;
-};
-
-struct TdoArena {
-    struct TdoArenaNode *first;
-    struct TdoArenaNode *latest;
-    void const *last_allocation;
-};
+#include "arena.h"
 
 struct TdoArena *tdo_arena_init(size_t initial_capacity) {
     size_t header_size = sizeof(struct TdoArena) + sizeof(struct TdoArenaNode);
