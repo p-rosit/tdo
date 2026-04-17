d81bb12a7b5b1fb1cabf79ee77d80aaf84043826
Pontus <pontus.rosqvist@gmail.com>
2026-04-17 18:00:14 +0200

    the specific integer is implementation defined!

--- test/test_runner.py
+++ test/test_runner.py
@@ -56,7 +56,7 @@ def test_aborts(library: str, run_tests: Callable[[str], Tuple[list, str]]):
         name='test_aborts',
         duration=pytest.approx(0.0, abs=100.0),
         step=StepTest(file=library, name='test_aborts'),
-        signal=pytest.approx(0, abs=1e12),  # the specific signal integer is implementation defined?
+        signal=pytest.approx(0, abs=1e12),  # the specific signal integer is implementation defined
         stdout='',
         stderr='',
     )]
@@ -129,7 +129,7 @@ def test_all(library: str, run_tests: Callable[[str], Tuple[list, str]]):
             name='test_aborts',
             duration=pytest.approx(0.0, abs=100.0),
             step=StepTest(file=library, name='test_aborts'),
-            signal=pytest.approx(0, abs=1e12),  # the specific signal integer is implementation defined?
+            signal=pytest.approx(0, abs=1e12),  # the specific signal integer is implementation defined
             stdout='',
             stderr='',
         ),
