f27c98fa0e83d6242a3b1bf8f2df4f5ae0c2b922
p-rosit <pontus.rosqvist@gmail.com>
2026-05-17 12:40:49 +0200

    also assert summary is correct

--- test/test_human.py
+++ test/test_human.py
@@ -3,13 +3,20 @@ from conftest import RunTests, ErrorCode, Error
 
 
 def test_success(library: str, run_tests: RunTests):
-    code, out, _ = run_tests.execute(f'test::{library}::test_success', args=['--format', 'human'])
+    code, out, err = run_tests.execute(f'test::{library}::test_success', args=['--format', 'human'])
     assert code == ErrorCode(code=Error.ok)
     assert strip_ansi(out) == '[  0%] .\n'
+    pattern = (
+        r'Reading tests from stdin:\n'
+        r'Running 1 tests\n'
+        r'Ran 1 tests in \d+\.\d{2} seconds:\n'
+        r'    success:   1/1\n'
+    )
+    assert re.fullmatch(pattern, strip_ansi(err)), err
 
 
 def test_early_exit(library: str, run_tests: RunTests):
-    code, out, _ = run_tests.execute(f'test::{library}::test_early_exit', args=['--format', 'human'])
+    code, out, err = run_tests.execute(f'test::{library}::test_early_exit', args=['--format', 'human'])
     assert code == ErrorCode(code=Error.ok)
     assert strip_ansi(out) == (
         f'[  0%] UNEXPECTED EXIT (4) {library}::test_early_exit\n'
@@ -17,10 +24,19 @@ def test_early_exit(library: str, run_tests: RunTests):
         'Captured stderr ----------------------------------------------------------------\n'
         '--------------------------------------------------------------------------------\n'
     )
+    pattern = (
+        r'Reading tests from stdin:\n'
+        r'Running 1 tests\n'
+        r'Ran 1 tests in \d+\.\d{2} seconds:\n'
+        r'    success:   0/1\n'
+        r'    failure:   1/1\n'
+        r'        exit:      1/1\n'
+    )
+    assert re.fullmatch(pattern, strip_ansi(err)), err
 
 
 def test_aborts(library: str, run_tests: RunTests):
-    code, out, _ = run_tests.execute(f'test::{library}::test_aborts', args=['--format', 'human'])
+    code, out, err = run_tests.execute(f'test::{library}::test_aborts', args=['--format', 'human'])
     assert code == ErrorCode(code=Error.ok)
     pattern = (
         r'\[  0%\] SIGNAL \(-?\d+\) ' + re.escape(library) + r'::test_aborts\n'
@@ -29,19 +45,38 @@ def test_aborts(library: str, run_tests: RunTests):
         r'--------------------------------------------------------------------------------\n'
     )
     assert re.fullmatch(pattern, strip_ansi(out)), out
+    pattern = (
+        r'Reading tests from stdin:\n'
+        r'Running 1 tests\n'
+        r'Ran 1 tests in \d+\.\d{2} seconds:\n'
+        r'    success:   0/1\n'
+        r'    failure:   1/1\n'
+        r'        signal:    1/1\n'
+    )
+    assert re.fullmatch(pattern, strip_ansi(err)), err
 
 
 def test_error(library: str, run_tests: RunTests):
-    code, out, _ = run_tests.execute(f'test::{library}::test_name', args=['--format', 'human'])
+    code, out, err = run_tests.execute(f'test::{library}::test_name', args=['--format', 'human'])
     assert code == ErrorCode(code=Error.ok)
 
     lines = strip_ansi(out).splitlines()
     assert f'[  0%] ERROR {library}::test_name' == lines[0]
     assert lines[1].startswith('    Could not load test:')
 
+    pattern = (
+        r'Reading tests from stdin:\n'
+        r'Running 1 tests\n'
+        r'Ran 1 tests in \d+\.\d{2} seconds:\n'
+        r'    success:   0/1\n'
+        r'    failure:   1/1\n'
+        r'        error:     1/1\n'
+    )
+    assert re.fullmatch(pattern, strip_ansi(err)), err
+
 
 def test_timeout(library: str, run_tests: RunTests):
-    code, out, _ = run_tests.execute(f'test::{library}::test_timeout', args=['--timeout', 0.01])
+    code, out, err = run_tests.execute(f'test::{library}::test_timeout', args=['--timeout', 0.01])
     assert code == ErrorCode(code=Error.ok)
     assert strip_ansi(out) == (
         f'[  0%] TIMEOUT {library}::test_timeout\n'
@@ -51,10 +86,19 @@ def test_timeout(library: str, run_tests: RunTests):
         'Some error\\n\n'
         '--------------------------------------------------------------------------------\n'
     )
+    pattern = (
+        r'Reading tests from stdin:\n'
+        r'Running 1 tests\n'
+        r'Ran 1 tests in \d+\.\d{2} seconds:\n'
+        r'    success:   0/1\n'
+        r'    failure:   1/1\n'
+        r'        timeout:   1/1\n'
+    )
+    assert re.fullmatch(pattern, strip_ansi(err)), err
 
 
 def test_success_line_break(library: str, run_tests: RunTests):
-    code, out, _ = run_tests.execute(90 * f"""
+    code, out, err = run_tests.execute(90 * f"""
         test::{library}::test_success
     """, args=['--format', 'human'])
     assert code == ErrorCode(code=Error.ok)
@@ -62,10 +106,17 @@ def test_success_line_break(library: str, run_tests: RunTests):
         '[  0%] ................................................................................\n'
         '[ 88%] ..........\n'
     )
+    pattern = (
+        r'Reading tests from stdin:\n'
+        r'Running 90 tests\n'
+        r'Ran 90 tests in \d+\.\d{2} seconds:\n'
+        r'    success:  90/90\n'
+    )
+    assert re.fullmatch(pattern, strip_ansi(err)), err
 
 
 def test_all(library: str, run_tests: RunTests):
-    code, out, _ = run_tests.execute(f"""
+    code, out, err = run_tests.execute(f"""
         test::{library}::test_success
         test::{library}::test_success
         test::{library}::test_success
@@ -92,10 +143,19 @@ def test_all(library: str, run_tests: RunTests):
         '--------------------------------------------------------------------------------\n'
         '[ 55%] .....\n'
     )
+    pattern = (
+        r'Reading tests from stdin:\n'
+        r'Running 11 tests\n'
+        r'Ran 11 tests in \d+\.\d{2} seconds:\n'
+        r'    success:   9/11\n'
+        r'    failure:   2/11\n'
+        r'        exit:      2/2\n'
+    )
+    assert re.fullmatch(pattern, strip_ansi(err)), err
 
 
 def test_all_verbose(library: str, run_tests: RunTests):
-    code, out, _ = run_tests.execute(f"""
+    code, out, err = run_tests.execute(f"""
         test::{library}::test_success
         test::{library}::test_success
         test::{library}::test_success
@@ -128,10 +188,19 @@ def test_all_verbose(library: str, run_tests: RunTests):
         f'[ 82%] SUCCESS {library}::test_success_with_other_stdout\n'
         f'[ 91%] SUCCESS {library}::test_success_with_stderr\n'
     )
+    pattern = (
+        r'Reading tests from stdin:\n'
+        r'Running 11 tests\n'
+        r'Ran 11 tests in \d+\.\d{2} seconds:\n'
+        r'    success:   9/11\n'
+        r'    failure:   2/11\n'
+        r'        exit:      2/2\n'
+    )
+    assert re.fullmatch(pattern, strip_ansi(err)), err
 
 
 def test_all_very_verbose(library: str, run_tests: RunTests):
-    code, out, _ = run_tests.execute(f"""
+    code, out, err = run_tests.execute(f"""
         test::{library}::test_success
         test::{library}::test_success
         test::{library}::test_success
@@ -194,6 +263,15 @@ def test_all_very_verbose(library: str, run_tests: RunTests):
         'Other thing\\n\n'
         '--------------------------------------------------------------------------------\n'
     )
+    pattern = (
+        r'Reading tests from stdin:\n'
+        r'Running 11 tests\n'
+        r'Ran 11 tests in \d+\.\d{2} seconds:\n'
+        r'    success:   9/11\n'
+        r'    failure:   2/11\n'
+        r'        exit:      2/2\n'
+    )
+    assert re.fullmatch(pattern, strip_ansi(err)), err
 
 
 def strip_ansi(out: str) -> str:
