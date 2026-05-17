6650d19c5589b13ecc61da09498f0a548a67105b
p-rosit <pontus.rosqvist@gmail.com>
2026-05-17 12:27:08 +0200

    test stuff separately

--- test/test_human.py
+++ test/test_human.py
@@ -8,26 +8,36 @@ def test_success(library: str, run_tests: RunTests):
     assert strip_ansi(out) == '[  0%] .\n'
 
 
-def test_success_repeat(library: str, run_tests: RunTests):
-    code, out, _ = run_tests.execute(f"""
-        test::{library}::test_success
-        test::{library}::test_success
-        test::{library}::test_success
-        test::{library}::test_success
-    """, args=['--format', 'human'])
-    assert code == ErrorCode(code=Error.ok)
-    assert strip_ansi(out) == '[  0%] ....\n'
-
-
-def test_success_line_break(library: str, run_tests: RunTests):
-    code, out, _ = run_tests.execute(90 * f"""
-        test::{library}::test_success
-    """, args=['--format', 'human'])
+def test_early_exit(library: str, run_tests: RunTests):
+    code, out, _ = run_tests.execute(f'test::{library}::test_early_exit', args=['--format', 'human'])
     assert code == ErrorCode(code=Error.ok)
     assert strip_ansi(out) == (
-        '[  0%] ................................................................................\n'
-        '[ 88%] ..........\n'
+        f'[  0%] UNEXPECTED EXIT (4) {library}::test_early_exit\n'
+        'Captured stdout ----------------------------------------------------------------\n'
+        'Captured stderr ----------------------------------------------------------------\n'
+        '--------------------------------------------------------------------------------\n'
     )
+
+
+def test_aborts(library: str, run_tests: RunTests):
+    code, out, _ = run_tests.execute(f'test::{library}::test_aborts', args=['--format', 'human'])
+    assert code == ErrorCode(code=Error.ok)
+    pattern = (
+        r'\[  0%\] SIGNAL \(-?\d+\) ' + re.escape(library) + r'::test_aborts\n'
+        r'Captured stdout ----------------------------------------------------------------\n'
+        r'Captured stderr ----------------------------------------------------------------\n'
+        r'--------------------------------------------------------------------------------\n'
+    )
+    assert re.fullmatch(pattern, strip_ansi(out)), out
+
+
+def test_error(library: str, run_tests: RunTests):
+    code, out, _ = run_tests.execute(f'test::{library}::test_name', args=['--format', 'human'])
+    assert code == ErrorCode(code=Error.ok)
+
+    lines = strip_ansi(out).splitlines()
+    assert f'[  0%] ERROR {library}::test_name' == lines[0]
+    assert lines[1].startswith('    Could not load test:')
 
 
 def test_timeout(library: str, run_tests: RunTests):
@@ -43,13 +53,23 @@ def test_timeout(library: str, run_tests: RunTests):
     )
 
 
+def test_success_line_break(library: str, run_tests: RunTests):
+    code, out, _ = run_tests.execute(90 * f"""
+        test::{library}::test_success
+    """, args=['--format', 'human'])
+    assert code == ErrorCode(code=Error.ok)
+    assert strip_ansi(out) == (
+        '[  0%] ................................................................................\n'
+        '[ 88%] ..........\n'
+    )
+
+
 def test_all(library: str, run_tests: RunTests):
     code, out, _ = run_tests.execute(f"""
         test::{library}::test_success
         test::{library}::test_success
         test::{library}::test_success
-        test::{library}::test
-        test::{library}::test_aborts
+        test::{library}::test_early_exit
         test::{library}::test_success_with_stdout
         test::{library}::test_early_exit
         test::{library}::test_success
@@ -61,18 +81,16 @@ def test_all(library: str, run_tests: RunTests):
     assert code == ErrorCode(code=Error.ok)
     assert strip_ansi(out) == (
         '[  0%] ...\n'
-        f'[ 25%] ERROR {library}::test\n'
-        f'    Could not load test: {library}: undefined symbol: test\n'
-        f'[ 33%] SIGNAL (6) {library}::test_aborts\n'
+        f'[ 27%] UNEXPECTED EXIT (4) {library}::test_early_exit\n'
         'Captured stdout ----------------------------------------------------------------\n'
         'Captured stderr ----------------------------------------------------------------\n'
         '--------------------------------------------------------------------------------\n'
-        '[ 42%] .\n'
-        f'[ 50%] UNEXPECTED EXIT (4) {library}::test_early_exit\n'
+        '[ 36%] .\n'
+        f'[ 45%] UNEXPECTED EXIT (4) {library}::test_early_exit\n'
         'Captured stdout ----------------------------------------------------------------\n'
         'Captured stderr ----------------------------------------------------------------\n'
         '--------------------------------------------------------------------------------\n'
-        '[ 58%] .....\n'
+        '[ 55%] .....\n'
     )
 
 
@@ -81,8 +99,7 @@ def test_all_verbose(library: str, run_tests: RunTests):
         test::{library}::test_success
         test::{library}::test_success
         test::{library}::test_success
-        test::{library}::test
-        test::{library}::test_aborts
+        test::{library}::test_early_exit
         test::{library}::test_success_with_stdout
         test::{library}::test_early_exit
         test::{library}::test_success
@@ -94,24 +111,22 @@ def test_all_verbose(library: str, run_tests: RunTests):
     assert code == ErrorCode(code=Error.ok)
     assert strip_ansi(out) == (
         f'[  0%] SUCCESS {library}::test_success\n'
-        f'[  8%] SUCCESS {library}::test_success\n'
-        f'[ 17%] SUCCESS {library}::test_success\n'
-        f'[ 25%] ERROR {library}::test\n'
-        f'    Could not load test: {library}: undefined symbol: test\n'
-        f'[ 33%] SIGNAL (6) {library}::test_aborts\n'
+        f'[  9%] SUCCESS {library}::test_success\n'
+        f'[ 18%] SUCCESS {library}::test_success\n'
+        f'[ 27%] UNEXPECTED EXIT (4) {library}::test_early_exit\n'
         'Captured stdout ----------------------------------------------------------------\n'
         'Captured stderr ----------------------------------------------------------------\n'
         '--------------------------------------------------------------------------------\n'
-        f'[ 42%] SUCCESS {library}::test_success_with_stdout\n'
-        f'[ 50%] UNEXPECTED EXIT (4) {library}::test_early_exit\n'
+        f'[ 36%] SUCCESS {library}::test_success_with_stdout\n'
+        f'[ 45%] UNEXPECTED EXIT (4) {library}::test_early_exit\n'
         'Captured stdout ----------------------------------------------------------------\n'
         'Captured stderr ----------------------------------------------------------------\n'
         '--------------------------------------------------------------------------------\n'
-        f'[ 58%] SUCCESS {library}::test_success\n'
-        f'[ 67%] SUCCESS {library}::test_success\n'
-        f'[ 75%] SUCCESS {library}::test_success\n'
-        f'[ 83%] SUCCESS {library}::test_success_with_other_stdout\n'
-        f'[ 92%] SUCCESS {library}::test_success_with_stderr\n'
+        f'[ 55%] SUCCESS {library}::test_success\n'
+        f'[ 64%] SUCCESS {library}::test_success\n'
+        f'[ 73%] SUCCESS {library}::test_success\n'
+        f'[ 82%] SUCCESS {library}::test_success_with_other_stdout\n'
+        f'[ 91%] SUCCESS {library}::test_success_with_stderr\n'
     )
 
 
@@ -120,8 +135,7 @@ def test_all_very_verbose(library: str, run_tests: RunTests):
         test::{library}::test_success
         test::{library}::test_success
         test::{library}::test_success
-        test::{library}::test
-        test::{library}::test_aborts
+        test::{library}::test_early_exit
         test::{library}::test_success_with_stdout
         test::{library}::test_early_exit
         test::{library}::test_success
@@ -136,47 +150,45 @@ def test_all_very_verbose(library: str, run_tests: RunTests):
         'Captured stdout ----------------------------------------------------------------\n'
         'Captured stderr ----------------------------------------------------------------\n'
         '--------------------------------------------------------------------------------\n'
-        f'[  8%] SUCCESS {library}::test_success\n'
+        f'[  9%] SUCCESS {library}::test_success\n'
         'Captured stdout ----------------------------------------------------------------\n'
         'Captured stderr ----------------------------------------------------------------\n'
         '--------------------------------------------------------------------------------\n'
-        f'[ 17%] SUCCESS {library}::test_success\n'
+        f'[ 18%] SUCCESS {library}::test_success\n'
         'Captured stdout ----------------------------------------------------------------\n'
         'Captured stderr ----------------------------------------------------------------\n'
         '--------------------------------------------------------------------------------\n'
-        f'[ 25%] ERROR {library}::test\n'
-        f'    Could not load test: {library}: undefined symbol: test\n'
-        f'[ 33%] SIGNAL (6) {library}::test_aborts\n'
+        f'[ 27%] UNEXPECTED EXIT (4) {library}::test_early_exit\n'
         'Captured stdout ----------------------------------------------------------------\n'
         'Captured stderr ----------------------------------------------------------------\n'
         '--------------------------------------------------------------------------------\n'
-        f'[ 42%] SUCCESS {library}::test_success_with_stdout\n'
+        f'[ 36%] SUCCESS {library}::test_success_with_stdout\n'
         'Captured stdout ----------------------------------------------------------------\n'
         'Printed\\n\n'
         'Captured stderr ----------------------------------------------------------------\n'
         '--------------------------------------------------------------------------------\n'
-        f'[ 50%] UNEXPECTED EXIT (4) {library}::test_early_exit\n'
+        f'[ 45%] UNEXPECTED EXIT (4) {library}::test_early_exit\n'
         'Captured stdout ----------------------------------------------------------------\n'
         'Captured stderr ----------------------------------------------------------------\n'
         '--------------------------------------------------------------------------------\n'
-        f'[ 58%] SUCCESS {library}::test_success\n'
+        f'[ 55%] SUCCESS {library}::test_success\n'
         'Captured stdout ----------------------------------------------------------------\n'
         'Captured stderr ----------------------------------------------------------------\n'
         '--------------------------------------------------------------------------------\n'
-        f'[ 67%] SUCCESS {library}::test_success\n'
+        f'[ 64%] SUCCESS {library}::test_success\n'
         'Captured stdout ----------------------------------------------------------------\n'
         'Captured stderr ----------------------------------------------------------------\n'
         '--------------------------------------------------------------------------------\n'
-        f'[ 75%] SUCCESS {library}::test_success\n'
+        f'[ 73%] SUCCESS {library}::test_success\n'
         'Captured stdout ----------------------------------------------------------------\n'
         'Captured stderr ----------------------------------------------------------------\n'
         '--------------------------------------------------------------------------------\n'
-        f'[ 83%] SUCCESS {library}::test_success_with_other_stdout\n'
+        f'[ 82%] SUCCESS {library}::test_success_with_other_stdout\n'
         'Captured stdout ----------------------------------------------------------------\n'
         'other\\n\n'
         'Captured stderr ----------------------------------------------------------------\n'
         '--------------------------------------------------------------------------------\n'
-        f'[ 92%] SUCCESS {library}::test_success_with_stderr\n'
+        f'[ 91%] SUCCESS {library}::test_success_with_stderr\n'
         'Captured stdout ----------------------------------------------------------------\n'
         'Captured stderr ----------------------------------------------------------------\n'
         'Other thing\\n\n'
